3debb97a33ce361125da5602fa10034897117604
Nischal Rai <nischalpilmong@yahoo.com>
2020-03-29 14:21:59 +0200

    check if the player won

--- js/app.js
+++ js/app.js
@@ -8,14 +8,52 @@ $(document).ready(function(){
        else{
          if(player == 1){
            squareSelected.addClass('ex');
-
-           player = 2;
+           if(checkIfPlayerWon('ex')){
+             alert('Congrats! Player' + player + 'has won!');
+           }
+           else{
+              player = 2;
+           }
          }
          else{
            squareSelected.addClass('oh');
-           player = 1;
+           if(checkIfPlayerWon('oh')){
+             alert('Congrats! Player' + player + 'has won!');
+           }
+           else{
+              player = 1;
+           }
          }
        }
    });
-   
+   function checkIfPlayerWon(symbol){
+     //symbol is either 'ex' or 'oh'
+     if($('.sq1').hasClass(symbol) && $('.sq2').hasClass(symbol) && $('.sq3').hasClass(symbol)){
+         return true;
+     }
+     else if($('.sq4').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq6').hasClass(symbol)){
+         return true;
+     }
+     else if($('.sq7').hasClass(symbol) && $('.sq8').hasClass(symbol) && $('.sq9').hasClass(symbol)){
+        return true;
+     }
+     else if($('.sq1').hasClass(symbol) && $('.sq4').hasClass(symbol) && $('.sq7').hasClass(symbol)){
+         return true;
+     }
+     else if($('.sq2').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq8').hasClass(symbol)){
+         return true;
+     }
+     else if($('.sq3').hasClass(symbol) && $('.sq6').hasClass(symbol) && $('.sq9').hasClass(symbol)){
+        return true;
+     }
+     else if($('.sq1').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq9').hasClass(symbol)){
+        return true;
+     }
+     else if($('.sq3').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq7').hasClass(symbol)){
+        return true;
+     }
+     else{
+       return false;
+     }
+   }
 });
