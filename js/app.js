a00b7c0030ae9715dbe72bd58022f7f9b15cf6c9
Nischal Rai <nischalpilmong@yahoo.com>
2020-03-29 13:59:00 +0200

    made the square selection and add respective classes turnwise

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
-
+   let player = 1;
+   $('.square').on('click',function(event){
+      var squareSelected = $(this);
+       if(squareSelected.hasClass('ex') || squareSelected.hasClass('oh')){
+         alert('This square has already been selected.Please select another..!');
+       }
+       else{
+         if(player == 1){
+           squareSelected.addClass('ex');
+           player = 2;
+         }
+         else{
+           squareSelected.addClass('oh');
+           player = 1;
+         }
+       }
+   });
 });
-
-
-
